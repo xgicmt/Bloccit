7cb85ae0c553a576736d0ee8ff1ee9867da25c35
xgicmt <xgicmt@gmail.com>
2015-03-18 15:18:28 +0000

    Update Readme, Bash issues abound

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'faker'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -46,7 +47,7 @@ group :development, :test do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
-  howgem 'pry-rails'
+  gem 'pry-rails'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+require 'faker'
+
+#Create Posts
+  50.times do
+    Post.create!(
+      title: Faker::Lorem.sentence,
+      body:  Faker::Lorem.paragraph
+    )
+  end
+  
+ # def create_original_post(title, post)
+ #   if Post.exists?(title, post)
+ #     puts "That post already exists"
+ #   else
+ #   Post.create!(title: title, body: post)
+ #   end
+ #  end
+    
+posts = Post.all
+  
+  #create Comments
+  100.times do
+    Comment.create!(
+      post: posts.sample,
+      body: Faker::Lorem.paragraph
+      )
+  end
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
+
+
+
+
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
