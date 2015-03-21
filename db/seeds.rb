b4d3b6454769697fb1e6dd2e955a666fbd3013c7
xgicmt <xgicmt@gmail.com>
2015-03-21 05:06:39 +0000

    Added users, and associated users with comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
 require 'faker'
 Post.destroy_all
-#User.destroy_all
+User.destroy_all
 Comment.destroy_all
 
-def create_original_post(title, post)
-  if Post.exists?(title: title)
-    puts "That post already exists"  
-  else
-    Post.create!(title: title, body: post)
-  end
+5.times do
+  user = User.new(
+    name:      Faker::Name.name,
+    email:     Faker::Internet.email,
+    password:  Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
 end
+users = User.all
+
+#def create_original_post(user, title, post)
+#  if Post.exists?(title: title)
+#    puts "That post already exists"  
+#  else
+#    Post.create!(user: users.sample, title: title, body: post)
+#  end
+#end
 
 
 #Create Posts
@@ -18,9 +29,10 @@ end
 
   50.times do
     
-    create_original_post(
-      Faker::Lorem.sentence,
-      Faker::Lorem.paragraph
+    Post.create!(
+      user: users.sample,
+      title: Faker::Lorem.sentence,
+      body:  Faker::Lorem.paragraph
     )
   end
     
@@ -29,12 +41,20 @@ posts = Post.all
   #create Comments
   100.times do
     Comment.create!(
+      #user: users.sample,
       post: posts.sample,
       body: Faker::Lorem.paragraph
       )
   end
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'xgicmt@gmail.com', 
+  password: 'JakeandKate'
+  )
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 
