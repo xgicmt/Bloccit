e31951f28cfa2aa1ed80a5ee23e07de56812a07b
xgicmt <xgicmt@gmail.com>
2015-03-23 13:49:11 +0000

    Created Custom Scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   
+  def self.tester
+    order("posts.updated_at DESC")
+  end
+  
   default_scope { order('created_at DESC')}
+  scope :ordered_by_title, -> { order('title ASC')}
+  scope :ordered_by_reverse_created_at, -> { order('created_at DESC')}
+ # default_scope {order('created_at ASC')}
+ # default_scope {order('title ASC')}
+ # Post.find_by_user('Herman Becker IV')
+
 end
