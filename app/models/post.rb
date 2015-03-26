7ad60e954d95cca3f16bc7bae883555cec3844f4
xgicmt <xgicmt@gmail.com>
2015-03-26 02:33:42 +0000

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
     order("posts.updated_at DESC")
   end
   
-  default_scope { order('created_at DESC')}
+  #default_scope { order('created_at DESC')}
   scope :ordered_by_title, -> { order('title ASC')}
   scope :ordered_by_reverse_created_at, -> { order('created_at DESC')}
  # default_scope {order('created_at ASC')}
