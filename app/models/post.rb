df494e54e05f54744e62701a249fb1e8f03c65c7
xgicmt <xgicmt@gmail.com>
2015-03-29 23:53:21 -0500

    Complete uploading images assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :topic
+  mount_uploader :image, ImageUploader
   
   default_scope { order('created_at DESC')}
 
-validates :title, length: { minimum: 5 }, presence: true
-validates :body, length: {minimum: 20 }, presence: true
-validates :topic, presence: true
-validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: {minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
 end
