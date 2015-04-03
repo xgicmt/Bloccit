aeaccce3ba73352842166f1009a37229ca94eda6
xgicmt <xgicmt@gmail.com>
2015-04-03 00:09:50 -0500

    Completed down_vote spec, modified up_vote to stay DRY

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,13 +5,25 @@ describe VotesController do
 	include Devise::TestHelpers
 	describe '#up_vote' do
 		it "adds an up-vote to the post" do
-			request.env["HTTP_REFERER"] = '/'
-			@user = authenticated_user
-			@post = associated_post
-			sign_in @user
+		load_user
 			expect{
 				post( :up_vote, post_id: @post.id)
 			}.to change{ @post.up_votes }.by 1
 		end
 	end
+	describe '#down_vote' do
+		it "adds a down-vote to the post" do
+		load_user
+			expect{
+				post( :down_vote, post_id: @post.id)
+			}.to change{ @post.down_votes }.by 1
+		end
+	end
+
+      def load_user
+			request.env["HTTP_REFERER"] = '/'
+			@user = authenticated_user
+			@post = associated_post
+			sign_in @user
+      end
 end
