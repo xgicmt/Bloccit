dbe282be0af87982e085af7a317ebc986784539d
xgicmt <xgicmt@gmail.com>
2015-04-03 21:54:59 -0500

    Incomplete facoring assignment with tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,23 @@ require 'rails_helper'
 
 	describe User do
 	  include TestFactories
-
-	dscribe "#favorited(post)" do
+    
+      before do 
+        @post= associated_post
+        @user = authenticated_user
+      end
+	describe "#favorited(post)" do
+		
 	  xit "returns 'nil' if the user has not favorited the post" do
+	  	#expect(@user.email_favorites).to eq(nil)
+	  	expect(@user.favorites.find_by_post_id(@post)).to eq(nil)
 	  end
 
 	  xit "returns the appropriate favorite if it exists" do 
+	  	expect().to eq(post_id)
+	  end
+
+	  xit "returns 'nil' if the user has favorited another post" do
 	  end
 
 	end
