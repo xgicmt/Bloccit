46c19d076c921f07e7fa1a8e393392bf395ae0d4
xgicmt <xgicmt@gmail.com>
2015-04-04 15:52:38 -0500

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,19 @@ module ApplicationHelper
 	  (redcarpet.render markdown).html_safe
 	end
 
+	def up_vote_link_classes(post)
+	  if current_user.voted(post) && current_user.voted(post).up_vote?
+	  	"voted"
+	  else
+	  	''
+	  end
+	end
+
+	def down_vote_link_classes(post)
+      if current_user.voted(post) && current_user.voted(post).down_vote?
+	  	"voted"
+	  else
+	  	''
+	  end
+	end
 end
