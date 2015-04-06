49d12adf32fe809658152010d9f9537b407782ee
xgicmt <xgicmt@gmail.com>
2015-04-05 23:43:53 -0500

    Complete destroying with Ajax Checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,11 +33,15 @@ class CommentsController < ApplicationController
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment could not be deleted, Please try again."
-      redirect_to [@topic, @post]
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
 private
